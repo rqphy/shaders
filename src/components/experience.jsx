391f78e92cc0c4b14ef0133ce7e9b510b621a905
rqphy <raphael.ferreira@hetic.net>
2023-04-13 09:34:33 +0200

    :sparkles: add lights

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber"
 export default function Experience() {
 	return (
 		<Canvas>
+			<pointLight position={[10, 10, 10]} />
 			<mesh>
 				<planeBufferGeometry args={[3, 5]} />
 				<meshStandardMaterial color="red" />
